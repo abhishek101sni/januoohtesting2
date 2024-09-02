17620caa0e88d4e26dfb5fb59fb656956ab1be1c
JanusKoncepts <januskprojects@gmail.com>
2024-09-02 18:58:00 +0530

    new commit

--- src/assets/json/newDesign/OurProcessData.js
+++ src/assets/json/newDesign/OurProcessData.js
@@ -14,33 +14,33 @@ export const OurProcessData = [
   {
     id: "1",
     image: Search,
-    title: "Find",
+    title: "Find Us",
     poylgonImg: polygon1,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ",
+      "Want the best outdoor advertising for your brand?We’re here!",
   },
   {
     id: "2",
     image: call,
-    title: "Call To Us",
+    title: "Call Us",
     poylgonImg: polygon2,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ",
+      "Want to know what we offer more? Don’t wait! Call right away.",
   },
   {
     id: "3",
     image: Plan,
-    title: "Plan",
+    title: "Plan With Us",
     poylgonImg: polygon3,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ",
+      "Want to take it further? Let’s connect to plan!",
   },
   {
     id: "4",
     image: Buy,
     poylgonImg: polygon4,
-    title: "Buy",
+    title: "Buy With Us",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ",
+      "Want to plan effectively? Buy the best outdoor ad spaces with us!",
   },
 ];
